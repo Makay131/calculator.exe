d5e3fae8499bae3ac80d4c128f06ce13a2e4247e
Mahmut Enes Akay <mahmutenesakay9@gmail.com>
2022-12-05 09:26:46 +0300

    cleaner-code

---
+++ src/mixins/prepareCalc.js
@@ -0,0 +1,78 @@
+import config from '../config/errors.json'
+
+export default {
+    data() {
+        return {
+            config
+        }
+    },
+    watch: {
+        valueVisible(nw, old) {
+          ['+', '÷', 'x', '-', '.'].map((op,i, arr)=> {
+          if(nw === old + op && (nw.at(-1) === op && nw.at(-2) === op)) this.valueVisible = old;
+          if(nw.at(-1) === op && arr.includes(nw.at(-2))) this.valueVisible = old;
+          });
+    
+          if(nw.includes('.') && this.valueVisible.lastIndexOf('.') > this.valueVisible.indexOf('.') && this.checkDotUsageRepeatedly()) this.valueVisible = old;
+          if(nw.includes('%') && nw.lastIndexOf('%') !== -1 && nw.lastIndexOf('%') === nw.length-1) {
+            let oldVal = (+old / 100).toString();
+            this.clear();
+            if(isNaN(oldVal)) {
+              this.showError(config.percent.title, config.percent.description)
+            } else
+            this.valueVisible = oldVal;
+          }
+    
+          //for 0000 bug
+          if(this.valueVisible.at(0) === '0' && !this.valueVisible.includes('.') && nw === old + '0' && old !== '') {
+            this.valueVisible = old;
+          }
+        },
+        actualCalc(nw) {
+          if(!isFinite(nw)) this.showError(config.forever.title, config.forever.description);
+        }
+      },
+      methods: {
+        makeCharWork(char) {
+            switch(char) {
+              case '()':
+              if(
+                this.valueVisible.indexOf('(') == -1 ||
+                this.valueVisible.indexOf('(') != -1 &&
+                this.valueVisible.indexOf(')') != -1 &&
+                this.valueVisible.lastIndexOf('(') < this.valueVisible.lastIndexOf(')')
+              ) this.valueVisible += '(';
+              else if(
+                this.valueVisible.indexOf('(') != -1 &&
+                this.valueVisible.indexOf(')') == -1 ||
+                this.valueVisible.indexOf('(') != -1 &&
+                this.valueVisible.indexOf(')') != -1 &&
+                this.valueVisible.lastIndexOf('(') > this.valueVisible.lastIndexOf(')')
+              ) this.valueVisible += ')';
+      
+              if(!isNaN(this.valueVisible.at(this.valueVisible.lastIndexOf('(')-1))) {
+                this.valueVisible = this.valueVisible.slice(0, this.valueVisible.length-1) + 'x(';
+              }
+              break;
+              case '.':
+              if(this.valueVisible.length == 1)
+                this.valueVisible = '0.';
+              if(this.valueVisible.at(-2) === '(')
+                this.valueVisible = this.valueVisible.slice(0,-1) + '0.'
+              break;
+              case '+/-':
+              this.valueVisible === '' ? this.valueVisible += '(-' : this.valueVisible === '(-' ? this.clear() : '';
+              ['+', '-', 'x', '÷'].includes(this.valueVisible.at(-1)) && !this.valueVisible.includes('(-') ? this.valueVisible += '(-' : '';
+             if(!isNaN(+this.valueVisible)) {
+              let val = this.valueVisible;
+              this.clear();
+              this.valueVisible = '(-' + val;
+             }
+             break;
+             case 'C':
+             this.clear();
+             break;
+            }
+          }
+      }
+}
